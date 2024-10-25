0c633ae2dafd2ee1ad951791c3ee7743ed42023e
maksymis <32574056+maksymiuks@users.noreply.github.com>
2024-10-25 16:30:23 +0200

    Enhance debug on failures

--- R/install.R
+++ R/install.R
@@ -22,8 +22,13 @@ install_packages_process <- R6::R6Class(
           tryCatch(
             utils::install.packages(...),
             warning = function(w) {
-              if (escalate_warning(w)) stop(w$message)
-              else warning(w)
+              if (escalate_warning(w)) {
+                print(w$message)
+                stop(w$message)
+              } else {
+                print(w$message)
+                warning(w)
+              }
             }
           )
         },
@@ -90,7 +95,9 @@ is_install_failure_warning <- function(w) {
     "(dependenc|package).*(is|are) not available",
     "installation of package.*had non-zero exit status",
     "installation of one or more packages failed",
-    "cannot open compressed file"
+    "cannot open compressed file",
+    "(C|c)ouldn't connect to server",
+    "Timeout.*was reached"
   )
 
   re <- paste0("(", paste0(patterns, collapse = "|"), ")")
