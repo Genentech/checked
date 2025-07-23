b2c87773ee91175b6a2bfee799adba372a3f5c5e
maksymis <32574056+maksymiuks@users.noreply.github.com>
2025-07-23 12:40:25 +0200

    Refactor results generation

--- R/options.R
+++ R/options.R
@@ -3,6 +3,17 @@ options::define_options(
   "tty refresh interval when reporting results in milliseconds",
   tty_tick_interval = 0.1,
 
+  "character vector indicating whether R error should be thrown when issues
+   are discovered when generating results. \"never\" means that no errors
+   are thrown. If \"issues\" then errors are emitted only on issues, whereas
+   \"potential issues\" stands for error on both issues and potential issues.",
+  results_error_on = "never",
+
+  "character vector indicating which packages should be included in the results.
+   \"all\" means that all packages are kept. If \"issues\" then only packages 
+   with issues identified are presented.",
+  results_keep = "all",
+
   "`logical` indicating whether output directory should be unlinked before
    running checks. If `FALSE`, an attempt will me made to restore previous
    progress from the same `output`",
